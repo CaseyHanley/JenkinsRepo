b1640d5cbe9ff47ce38f8f37aa3d1a029ca71520
Casey.T.Hanley <caseythomashanley@hotmail.com>
2023-03-30 12:55:12 -0500

    made some test cases

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+
+    @Test
+    void main() {
+
+        System.out.println("Hello World");
+    }
+}
